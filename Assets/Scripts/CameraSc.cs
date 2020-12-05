6c352535ff5cb3b3371ab53f6f7621229248d9f9
PC\Beka <beka.pukhashvili@gmail.com>
2020-12-06 00:38:31 +0400

    My changes To Physics

---
+++ Assets/Scripts/CameraSc.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSc : MonoBehaviour
+{
+
+    RocketShip ship;
+    [SerializeField] float locationY = 5;
+    [SerializeField] float cameraTimeDelay;
+
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ship = FindObjectOfType<RocketShip>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameralocXY = new Vector3(ship.transform.position.x, ship.transform.position.y+locationY, transform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position, cameralocXY, ref velocity, cameraTimeDelay);
+        
+
+
+    }
+}
--- Assets/Scripts/RocketShip.cs
+++ Assets/Scripts/RocketShip.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class RocketShip : MonoBehaviour
 {
-
+    [SerializeField] float turning = 20;
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -24,30 +24,53 @@ public class RocketShip : MonoBehaviour
 
     private void ProcessInput()
     {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
-            
-            rigidBody.AddRelativeForce(Vector3.up*Time.deltaTime);
 
-            if(!audioSource.isPlaying)
+            rigidBody.AddRelativeForce(Vector3.up * Time.deltaTime);
+
+            if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
         }
+
         else
         {
             audioSource.Stop();
         }
+    }
 
-            if (Input.GetKey(KeyCode.A))
+    private void Rotate()
+    {
+        //rigidBody.freezeRotation = true;
+
+
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(100*Vector3.forward*Time.deltaTime);
+            rigidBody.AddTorque(transform.forward*Time.deltaTime/turning);
+            
+            //transform.Rotate(100 * Vector3.forward * Time.deltaTime);
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-100 * Vector3.forward*Time.deltaTime);
+            rigidBody.AddTorque(-transform.forward*Time.deltaTime/turning);
+
+            //transform.Rotate(-100 * Vector3.forward * Time.deltaTime);
 
         }
+
+        //rigidBody.freezeRotation = false;
+
     }
+
+
 }
+    

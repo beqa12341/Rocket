22eaee4ba82f9d52fa21de00ea14f3328b22e2fa
PC\Beka <beka.pukhashvili@gmail.com>
2020-12-13 20:22:54 +0400

    Rotation Cycle Fix

--- Assets/Scripts/Oscilator.cs
+++ Assets/Scripts/Oscilator.cs
@@ -6,7 +6,7 @@ public class Oscilator : MonoBehaviour
 {
 
     [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
-    [SerializeField] float speed = 2;
+    [SerializeField] float period = 2f;
 
     float movementFactor;
     Vector3 startingPos;
@@ -16,11 +16,13 @@ public class Oscilator : MonoBehaviour
         startingPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
 
-        movementFactor = (Mathf.Sin(Time.time * speed));
+        movementFactor = rawSinWave / 2f+0.5f;
 
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
